bfb423feef2634fba096f0eac9d01f44269b861a
Craig Phillips <craig.phillips@cdk.com>
2015-02-27 17:32:31 +0000

    Add first impl o the runner executor and monitor

--- lib/bashcat/runner.py
+++ lib/bashcat/runner.py
@@ -2,13 +2,39 @@
 
 # Copyright (C) 2015 Craig Phillips.  All rights reserved.
 
+import os
+
+
 class Runner(object):
     def __init__(self, config):
+        self._pid = os.getpid()
         self._config = config
         self._exitcode = 255
 
+        
+    def monitor(self, r):
+        fr = os.fdopen(r)
+        os.waitpid(pid, 0)
+
+
+    def executor(self, w):
+        os.environ['BASHCAT_FD'] = w
+        os.execv("/bin/bash", [ self._config['script'] ] + self._config['script_args'])
+
+
     def run(self):
-        pass
+        r, w = os.pipe()
+        pid = os.fork()
+
+        if pid == 0:
+            # Child
+            os.close(r)
+            self.executor(w)
+
+        # Parent
+        os.close(w)
+        self.monitor(r)
+
 
     @property
     def exitcode(self):
