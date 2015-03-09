067b638b182063be3cc468e4b2478cb6367d6777
Craig Phillips <craig.phillips@cdk.com>
2015-03-09 08:35:35 +0000

    Convert to string and output stack trace in debug mode.

--- lib/bashcat/output.py
+++ lib/bashcat/output.py
@@ -5,6 +5,14 @@
 import os, sys
 
 def err(msg):
+    if not str(msg):
+        msg = repr(msg)
+
+    if int(os.environ.get('DEBUG', 0)):
+        (exc_type, exc_val, exc_tb) = sys.exc_info()
+        import traceback
+        traceback.print_tb(exc_tb)
+
     sys.stderr.write("{prog}: error: {msg}\n".format(
         prog=os.path.basename(sys.argv[0]),
         msg=str(msg)
